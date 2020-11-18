e7ae1556945e42bfde7fdf5d584fd36e7a84cdd1
jw59 <joshw59@gmail.com>
2020-11-17 20:49:45 -0500

    pr2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 ########################################################################################################################
 ### Do not forget to adjust the following variables to your own plugin.
 
-## Touch for PR
+## Touch for PR2
 
 # The plugin's identifier, has to be unique
 plugin_identifier = "googledrivebackup"
